9be37646bd899355191a0f27308350cfb1d5d31e
peterd100 <146393096+peterd100@users.noreply.github.com>
2025-02-09 21:32:05 -0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -6,13 +6,13 @@
 #include <fstream>
 
 #define SERVER_PORT 8080
-#define BUFFER_SIZE 1024
+#define SIZE 1024
 
 using namespace std;
 
-void receive_file(int sockfd, struct sockaddr_in &clientAddr, socklen_t &addrLen) {
+void receive_file(int serverSocket, struct sockaddr_in &clientAddr, socklen_t &addrlen) {
     ofstream outFile("received.bmp", ios::binary);
-    char buffer[BUFFER_SIZE];
+    char buffer[SIZE];
     ssize_t recvLen;
 
     if (!outFile) {
@@ -21,46 +21,42 @@ void receive_file(int sockfd, struct sockaddr_in &clientAddr, socklen_t &addrLen
     }
 
     cout << "Receiving file..." << endl;
-    while ((recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddr, &addrLen)) > 0) {
+    while ((recvLen = recvfrom(serverSocket, buffer, SIZE, 0, (struct sockaddr *)&clientAddr, &addrlen)) > 0) {
         outFile.write(buffer, recvLen);
-        if (recvLen < BUFFER_SIZE) break;
+        if (recvLen < SIZE) break;
     }
     outFile.close();
     cout << "File received successfully." << endl;
 }
 
 int main() {
-    int sockfd;
-    char buffer[BUFFER_SIZE];
+    int serverSocket;
+    char buffer[SIZE];
     struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
+    socklen_t addrlen = sizeof(clientAddr);
 
-    // Create UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (serverSocket < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
-    // Configure server address
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(SERVER_PORT);
 
-    // Bind socket
-    if (bind(sockfd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (bind(serverSocket, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         perror("Bind failed");
-        close(sockfd);
+        close(serverSocket);
         return 1;
     }
 
     cout << "UDP Server listening on port " << SERVER_PORT << endl;
 
     while (true) {
-        // Receive message from client
-        ssize_t recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, 
-                                   (struct sockaddr *)&clientAddr, &addrLen);
+        ssize_t recvLen = recvfrom(serverSocket, buffer, SIZE, 0, 
+                                   (struct sockaddr *)&clientAddr, &addrlen);
         if (recvLen < 0) {
             perror("Receive failed");
             break;
@@ -69,15 +65,14 @@ int main() {
         cout << "Received from client (port " << ntohs(clientAddr.sin_port) << "): " << buffer << endl;
 
         if (strcmp(buffer, "START_FILE_TRANSFER") == 0) {
-            receive_file(sockfd, clientAddr, addrLen);
+            receive_file(serverSocket, clientAddr, addrlen);
             continue;
         }
 
-        // Echo message back to client
-        sendto(sockfd, buffer, recvLen, 0, (struct sockaddr *)&clientAddr, addrLen);
+        sendto(serverSocket, buffer, recvLen, 0, (struct sockaddr *)&clientAddr, addrlen);
         cout << "Echoed back to client (port " << ntohs(clientAddr.sin_port) << "): " << buffer << endl;
     }
 
-    close(sockfd);
+    close(serverSocket);
     return 0;
 }
